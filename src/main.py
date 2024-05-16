4936104d3708358c94c64e8cecef206b5ad4b415
agungatd <agungatidhira@gmail.com>
2024-05-16 15:56:44 +0700

    init commit

--- src/main.py
+++ src/main.py
@@ -1,14 +1,30 @@
+import os
+from dotenv import load_dotenv
 from snowflake.snowpark import Session
+import snowflake.snowpark.functions as F
+
+load_dotenv()
+
 connection_parameters = {
-  "account": "<your snowflake account>",
-  "user": "<your snowflake user>",
-  "password": "<your snowflake password>",
-  "role": "<your snowflake role>",  # optional
-  "warehouse": "<your snowflake warehouse>",  # optional
-  "database": "<your snowflake database>",  # optional
-  "schema": "<your snowflake schema>",  # optional
-}  
+  "account": "jd01396.ap-southeast-3.aws",
+  "user": "agungatd",
+  "password": os.environ.get('password')
+}
 
 new_session = Session.builder.configs(connection_parameters).create()
+user = new_session.get_current_user()
+print(user)
+
+new_session.sql("USE ROLE ACCOUNTADMIN").collect()
+new_session.sql("USE DATABASE TEST_DB").collect()
+new_session.sql("USE WAREHOUSE COMPUTE_WH").collect()
+
+diamonds_df = new_session.table("diamonds")
+# print(diamonds_df.show(5))
+
+diamonds_pdf = diamonds_df.to_pandas()
+
+expression = F.lower(F.col("CUT"))
+print(diamonds_df.select(expression).show(5))
 
 new_session.close()
